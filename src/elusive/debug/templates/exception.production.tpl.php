9dccd849b9dd5bacd9c58d76a7cbc4deaa0ef562
Roger Soucy <roger.soucy@elusive-concepts.com>
2020-03-28 13:27:20 -0400

    Updated production exception page

--- src/elusive/debug/templates/exception.production.tpl.php
+++ src/elusive/debug/templates/exception.production.tpl.php
@@ -4,6 +4,7 @@
 	<title>Server Error (500)</title>
 	<meta charset="UTF-8">
 
+	<link rel="stylesheet" type="text/css" href="https://framework.elusive-concepts.com/css/elusive-framework-icons.css">
 	<link rel="stylesheet" type="text/css" href="https://framework.elusive-concepts.com/css/ec_source.css">
 	<link rel="stylesheet" type="text/css" href="https://framework.elusive-concepts.com/css/exception.css">
 
@@ -14,8 +15,7 @@
 
 	<header>
 		<hgroup>
-			<h1><?php echo $this->request->data['ENV']['SERVER_NAME'] ?></h1>
-			<h2>Server Error</h2>
+			<h1><?= $this->request->data['ENV']['SERVER_NAME'] ?></h1>
 		</hgroup>
 	</header>
 
@@ -24,20 +24,21 @@
 		<h1 class="icon error">Server Error (500)</h1>
 
 		<div class="message">
-			<p>An internal server error has occurred on <strong><?php echo $this->request->data['ENV']['SERVER_NAME'] ?></strong> while trying to access the following url:</p>
+			<p>A server error has occurred on <strong><?= $this->request->data['ENV']['SERVER_NAME'] ?></strong> while trying to access the following url:</p>
 			<blockquote><a href="http://<?php echo $this->request->data['ENV']['SERVER_NAME'] . $this->request->data['ENV']['REQUEST_URI'] ?>">http://<?php echo $this->request->data['ENV']['SERVER_NAME'] . $this->request->data['ENV']['REQUEST_URI'] ?></a></blockquote>
+			<p>If the issue persists, please contact an administrator.</p>
 		</div>
 
 	</div><!-- #content -->
 
 	<footer>
+		&nbsp;
 	</footer>
 
 </div>
 
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://framework.elusive-concepts.com/js/ec_render_source.js"></script>
-<script type="text/javascript" src="https://framework.elusive-concepts.com/js/exception.js"</script>
+<script type="text/javascript" src="https://framework.elusive-concepts.com/js/exception.js"></script>
 
 </body>
 </html>
