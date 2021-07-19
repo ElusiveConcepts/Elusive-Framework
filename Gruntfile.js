b516360872fe8fd1b79da64dbc112576d6e887ac
Roger Soucy <roger.soucy@elusive-concepts.com>
2021-07-19 12:45:16 -0400

    removed grunt-phpdoc and updated grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,6 @@ module.exports = function(grunt)
 			}
 		},
 		clean: {
-			docs:    { src: [ 'docs' ] },
 			build:   { src: [ 'build' ] },
 			release: { src: [ 'docs/phpdoc-cache-*' ] }
 		},
@@ -20,30 +19,19 @@ module.exports = function(grunt)
 				options: { }
 			}
 		},
-		phpdoc: {
-			release: {
-				options: { },
-				src: [
-					'./src/elusive',
-					'./src/app'
-				],
-				dest: ['docs']
-			}
-		}
 	});
 
 	// Task Loading
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-mkdir');
-	grunt.loadNpmTasks('grunt-phpdoc');
 
 	// Task Definitions
 	grunt.registerTask('build', 'Builds the project into ./build',
 		[ 'clean:build', 'copy:build' ]
 	);
 	grunt.registerTask('release', 'Builds the project for release', 
-		[ 'clean:docs', 'phpdoc:release', 'clean:build', 'clean:release', 'copy:build' ]
+		[ 'clean:build', 'clean:release', 'copy:build' ]
 	);
 
 };
